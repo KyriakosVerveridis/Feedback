5c84910780b065578efd472adcbe5d3225fe59e3
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-11 19:06:05 +0200

    Introducing Modelforms

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,10 +1,23 @@
 from django import forms
+from .models import Review
 
-# ReviewForm: A simple Django form for collecting user reviews
-class ReviewForm(forms.Form):
-	user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
-		"required": "Your name must not be empty!",
-		"max_length": "Please enter a shorter name!"
-	})
-	review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
-	rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+# # ReviewForm: A simple Django form for collecting user reviews
+# class ReviewForm(forms.Form):
+# 	user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+# 		"required": "Your name must not be empty!",
+# 		"max_length": "Please enter a shorter name!"
+# 	})
+# 	review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+# 	rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    """
+    A Django ModelForm that automatically generates form fields 
+    based on the Review model.
+    """
+    class Meta:
+        model = Review # Specifies the model that this form is based on
+        fields = "__all__"
+        # exclude = ["owner_comment"]
+        
+        
