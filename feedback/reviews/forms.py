db82adf94e5eccb623ff203634243cfe67da1ac4
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-11 19:34:21 +0200

    Configuring the Modelform

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -19,5 +19,20 @@ class ReviewForm(forms.ModelForm):
         model = Review # Specifies the model that this form is based on
         fields = "__all__"
         # exclude = ["owner_comment"]
+		# Custom labels for form fields displayed in the UI
+        
+        labels = {
+            "user_name": "Your Name",
+            "review_text":"Your Feedback",
+            "rating":"Your Rating"
+		}
+        
+        # Custom validation error messages for specific fields
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty!",
+            	"max_length": "Please enter a shorter name!"
+			}
+		}
         
         
