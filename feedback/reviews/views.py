48f66186cf08289d27917846d9b938fd57b6ee72
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-12 12:39:26 +0200

    Showing a Detail Template

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -49,12 +49,49 @@ class ThankYouView(TemplateView):
 	
 
 class ReviewsListView(TemplateView):
-	template_name = "reviews/review_list.html"
+    """
+    Displays a list of all submitted reviews using 
+    a Django TemplateView.
+    """
+    template_name = "reviews/review_list.html"
+    
+    def get_context_data(self, **kwargs):
+        """
+        Extends the default context data with all Review instances.
+        """
+        # Get the base context from the parent TemplateView
+        context = super().get_context_data(**kwargs)
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		reviews = Review.objects.all()
-		context["reviews"] = reviews
-		return context
+        # Fetch all reviews from the database
+        reviews = Review.objects.all()
 
-	
+        # Add the reviews to the template context
+        context["reviews"] = reviews
+        return context
+    
+
+class SingleReviewView(TemplateView):
+    """
+    Displays the details of a single review using a Django TemplateView.
+    Retrieves the review based on its ID and passes it to the template.
+    """
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        """
+        Extends the default context data with 
+        the selected Review instance.
+        """
+        # Get the base context from the parent TemplateView
+        context = super().get_context_data(**kwargs)
+
+        # Retrieve the review ID from the URL parameters
+        review_id = kwargs["id"]
+
+        # Fetch the corresponding Review object from the database
+        selected_review = Review.objects.get(pk=review_id)
+
+        # Add the selected review to the context for template access
+        context["review"] = selected_review
+        return context
+
