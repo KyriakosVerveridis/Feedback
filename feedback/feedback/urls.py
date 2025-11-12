700c4250ab07e0a1aafd98b30603b7ea00ec6608
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-13 00:20:51 +0200

    File Uploads Starting Setup

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("reviews.urls")), # http://localhost:8000/
+    path("profiles/", include("profiles.urls")), 
 
 ]
